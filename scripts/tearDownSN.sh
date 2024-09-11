509ee07bdd23f6e41a64bb2196d4f253c553ef32
Gilbert Kwan <gkwan@ca.ibm.com>
2024-09-11 14:07:26 -0400

    update SN test scripts (#102)
    * update SN test scripts
    
    Signed-off-by: Gilbert Kwan <gkwan@ca.ibm.com>
    
    * update SN test scripts
    
    Signed-off-by: Gilbert Kwan <gkwan@ca.ibm.com>
    
    * update SN test scripts
    
    Signed-off-by: Gilbert Kwan <gkwan@ca.ibm.com>
    
    * update SN test scripts
    
    Signed-off-by: Gilbert Kwan <gkwan@ca.ibm.com>
    
    ---------
    
    Signed-off-by: Gilbert Kwan <gkwan@ca.ibm.com>

---
+++ scripts/tearDownSN.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+oc delete -f deploy.yaml
+oc delete imagestream.image.openshift.io/system-imagestream
+oc delete bc system-buildconfig
+sleep 10
+oc get routes
+oc get OpenLibertyApplications
+oc get imagestreams
+oc get all -l name=system
